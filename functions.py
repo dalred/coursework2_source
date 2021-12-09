0cd5a4dd26412f7443aae87ea95c6e8b06033330
dalred <RcrF63gGG7HpbAT>
2021-12-09 17:04:53 +0300

    add bookmarks

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import json
+import json, re
 def read_json(name):
     with open(name, "r", encoding='utf-8') as file:
         return json.load(file)
@@ -13,3 +13,52 @@ def counts_comments(items, comments_path):
             else:
                 comments_list_by_posts[comments['post_id']] += 1
     return comments_list_by_posts
+
+
+def search_tag(tag_name, profile_path):
+    tags = []
+    for item in read_json(profile_path):
+        if f"#{tag_name}" in item['content'].lower():
+            tags.append(item)
+    return tags
+
+def view_tag(profile_path):
+    tags = []
+    for i in read_json(profile_path):
+        for tag in i['content'].split():
+            if tag.startswith("#"):
+                tags.append(tag.lstrip("#").lower())
+    tags = set(tags)
+    return tags
+
+#Много времени убил на эту функцию не без сторонней помощи, такое ощущение
+#что можно было сделать в тысячу раз проще и есть какая-то красивая регулярка
+#Буду ждать решения от наставников
+#Так же интересно был ли какой-то смысл делать с ней что-то в шаблоне.
+def regex_tags(profile_path):
+    regexp = re.compile(r'(?<!>|\w)#{1}\w+')
+    profile = read_json(profile_path)
+    for item in profile:
+        while re.search(regexp, item['content']) is not None:
+            replacement = re.search(regexp, item['content'])[0][1:]
+            item['content'] = re.sub(regexp, f'<a href="/tag/{replacement}#{replacement}">#{replacement}</a>',
+                                     item['content'],
+                                     count=1)
+    return profile
+
+def serialize_sets(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    return obj
+
+def add_bookmarks(post_id, bookmarks_path):
+    data = read_json(bookmarks_path)
+    data.append(post_id)
+    with open(bookmarks_path, "w", encoding='utf-8') as f:
+        json.dump(set(data), f, default=serialize_sets)
+
+def delete_bookmarks(post_id, bookmarks_path):
+    data = read_json(bookmarks_path)
+    data.remove(post_id)
+    with open(bookmarks_path, "w", encoding='utf-8') as f:
+        json.dump(set(data), f, default=serialize_sets)
--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-import os
-from flask import Flask, render_template, send_from_directory, request
-from functions import read_json, counts_comments
+import os, re
+from flask import Flask, render_template, send_from_directory, request,redirect
+from functions import read_json, counts_comments, search_tag, view_tag, regex_tags, add_bookmarks, delete_bookmarks
 
 json_dir = 'data'
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 profile_path = os.path.join(BASEDIR, json_dir, 'data.json')
 comments_path = os.path.join(BASEDIR, json_dir, 'comments.json')
 images_path = os.path.join(BASEDIR, 'static\img')
+bookmarks_path = os.path.join(BASEDIR, json_dir, 'bookmarks.json')
 app = Flask(__name__)
 
 
 @app.route("/")
 def page_index():
-    profile = read_json(profile_path)
-    return render_template('index.html', profile=profile)
+    bookmarks__ = read_json(bookmarks_path)
+    profile = regex_tags(profile_path)
+    return render_template('index.html', profile=profile, count=len(bookmarks__), bookmarks=bookmarks__)
 
 
 @app.route("/posts/<int:post_id>")
 def posts(post_id):
     post = {}
-    for item in read_json(profile_path):
+    profile = regex_tags(profile_path)
+    for item in profile:
         if item['pk'] == post_id:
             post = item
     comments = [item for item in read_json(comments_path) if item['post_id'] == post_id]
@@ -49,6 +52,31 @@ def username(username):
     comments = counts_comments(items, comments_path)
     return render_template('user-feed.html', comments=comments, items=items)
 
+@app.route("/tag/<tagname>", methods=["GET"])
+def page_tag(tagname):
+    if not tagname:
+        return "Record not found", 400
+    items = search_tag(tagname, profile_path)
+    comments = counts_comments(items, comments_path)
+    return render_template('tag.html', items=items, comments=comments, tagname=tagname)
+
+@app.route("/bookmarks/add/<int:post_id>")
+def bookmarks_add(post_id):
+    add_bookmarks(post_id, bookmarks_path)
+    return redirect("/", code=302)
+
+@app.route("/bookmarks/")
+def bookmarks_():
+    data = read_json(bookmarks_path)
+    items = [item for item in read_json(profile_path) if item['pk'] in data]
+    comments = counts_comments(items, comments_path)
+    return render_template('bookmarks.html', items=items, comments=comments)
+
+
+@app.route("/bookmarks/remove/<int:post_id>")
+def delete_bookmarks_(post_id):
+    delete_bookmarks(post_id, bookmarks_path)
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run('127.0.0.1', 8000)
