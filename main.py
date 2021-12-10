293de2a25ae590aebf75c6ed2d7fa676100b9825
dalred <RcrF63gGG7HpbAT>
2021-12-11 02:29:26 +0300

    add file_id from another file

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, send_from_directory, request, redirect
 from functions import *
+from google_file_id import dict_file_id
 
 json_dir = 'data'
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 profile_path = os.path.join(BASEDIR, json_dir, 'data.json')
-
 images_path = os.path.join(BASEDIR, 'static\img')
-bookmarks_fileId = '1kIePmIBOeJkjBCQvNajl0WYvrIHtqF6w'
-comments_fileid = '1iCHQ7oDHWHeU9Cq-WB5h6xEJPlMrRPEd'
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def page_index():
-    bookmarks__ = read_google_json(bookmarks_fileId)
+    bookmarks__ = read_google_json(dict_file_id['bookmarks_fileId'])
     profile = read_json(profile_path)
     profile_html = regex_tags(profile)
     return render_template('index.html', profile=profile_html, count=len(bookmarks__), bookmarks=bookmarks__)
@@ -23,15 +22,15 @@ def posts(post_id):
     if request.method == "POST":
         comment = request.form.get('comment-form__textarea')
         name = request.form.get('comment-form__input')
-        add_comment(post_id, comment, name, comments_fileid)
+        add_comment(post_id, comment, name, dict_file_id['comments_fileid'])
     post = {}
     profile = read_json(profile_path)
     profile_html = regex_tags(profile)
     for item in profile_html:
         if item['pk'] == post_id:
             post = item
-    data = read_google_json(bookmarks_fileId)
-    comments = [item for item in read_google_json(comments_fileid) if item['post_id'] == post_id]
+    data = read_google_json(dict_file_id['bookmarks_fileId'])
+    comments = [item for item in read_google_json(dict_file_id['comments_fileid']) if item['post_id'] == post_id]
     return render_template('post.html', **post, comments=comments, count=len(comments), bookmarks=data)
 
 
@@ -45,7 +44,7 @@ def search_():
     try:
         search = request.args.get('s')
         items = [item for item in read_json(profile_path) if search in item['content']]
-        comments = counts_comments(items[:10], comments_fileid)
+        comments = counts_comments(items[:10], dict_file_id['comments_fileid'])
         return render_template('search.html', comments=comments, items=items, count=len(items), search=search)
     except:
         return '', 400
@@ -55,8 +54,8 @@ def search_():
 def username_(username):
     profile = read_json(profile_path)
     items = [item for item in regex_tags(profile) if item['poster_name'] == username]
-    comments = counts_comments(items, comments_fileid)
-    data = read_google_json(bookmarks_fileId)
+    comments = counts_comments(items, dict_file_id['comments_fileid'])
+    data = read_google_json(dict_file_id['bookmarks_fileId'])
     return render_template('user-feed.html', comments=comments, items=items, bookmarks=data)
 
 
@@ -66,27 +65,27 @@ def page_tag(tagname):
         return "Record not found", 400
     profile = read_json(profile_path)
     items = search_tag(tagname, profile)
-    comments = counts_comments(items, comments_fileid)
+    comments = counts_comments(items, dict_file_id['comments_fileid'])
     return render_template('tag.html', items=items, comments=comments, tagname=tagname)
 
 
 @app.route("/bookmarks/add/<int:post_id>")
 def bookmarks_add(post_id):
-    add_bookmarks(post_id, bookmarks_fileId)
+    add_bookmarks(post_id, dict_file_id['bookmarks_fileId'])
     return redirect("/", code=302)
 
 
 @app.route("/bookmarks/")
 def bookmarks_():
-    data = read_google_json(bookmarks_fileId)
+    data = read_google_json(dict_file_id['bookmarks_fileId'])
     items = [item for item in read_json(profile_path) if item['pk'] in data]
-    comments = counts_comments(items, comments_fileid)
+    comments = counts_comments(items, dict_file_id['comments_fileid'])
     return render_template('bookmarks.html', items=items, comments=comments)
 
 
 @app.route("/bookmarks/remove/<int:post_id>")
 def delete_bookmarks_(post_id):
-    delete_bookmarks(post_id, bookmarks_fileId)
+    delete_bookmarks(post_id, dict_file_id['bookmarks_fileId'])
     return redirect("/", code=302)
 
 
